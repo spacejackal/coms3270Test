028dec808b6ddc98ce7004eaf26bf1c5f971143c
Jacob Leary <jackal@iastate.edu>
2024-09-12 14:41:59 -0500

    reading all 3 numbers from the file

--- pointcloud.c
+++ pointcloud.c
@@ -5,7 +5,11 @@ int stat1() {
 	double highx, highy, highHeight;
 	double lowx, lowy, lowHeight;
 	scanf("%lf", &highx);
-	printf("this is the x location%lf/n",highx);
+	scanf("%lf", &highy);
+	scanf("%lf", &highHeight);
+	printf("this is the x location %lf\n",highx);
+	printf("this is the y location %lf\n", highy);
+	printf("this is the height of the location %lf\n", highHeight);
 
 	return 0;
 }
