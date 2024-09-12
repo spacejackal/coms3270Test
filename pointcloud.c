802ba0b06c55e9477e0f04c23c504aa814b71664
Jacob Leary <jackal@iastate.edu>
2024-09-12 15:29:07 -0500

    added include to pointcloud.c

--- pointcloud.c
+++ pointcloud.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-
-
-int stat1() {
-	double highx, highy;
-	double lowx, lowy;
-	double highHeight,lowHeight;
-	scanf("%lf", &highx);
-	scanf("%lf", &highy);
-	scanf("%lf", &highHeight);
-	
-	printf("this is the x location %lf\n",highx);
-	printf("this is the y location %lf\n", highy);
-	printf("this is the height of the location %lf\n", highHeight);
-	
-	return 0;
-}
+#include "pointcloud.h"
 
 int main() {
 	stat1();
 	return 1;
+}
+
+int stat1() {
+	double highx, highy;
+	double lowx, lowy;
+	double highHeight, lowHeight;
+	scanf("%lf", &highx);
+	scanf("%lf", &highy);
+	scanf("%lf", &highHeight);
+
+	printf("this is the x location %lf\n", highx);
+	printf("this is the y location %lf\n", highy);
+	printf("this is the height of the location %lf\n", highHeight);
+
+	return 0;
 }
